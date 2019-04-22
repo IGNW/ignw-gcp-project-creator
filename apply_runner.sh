936935302844492d2a11fdb5064a5e37fa42388f
Tom Chisholm <tomc@ignw.io>
2019-04-22 13:30:23 -0700

    runners

---
+++ apply_runner.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd terraform
+terraform init
+TF_VAR_org_id=<ORGID> TF_VAR_billing_account=<BILLINGID> terraform apply
---
+++ destroy_runner.sh
@@ -0,0 +1,6 @@
+
+#!/usr/bin/env bash
+
+cd terraform
+terraform init
+TF_VAR_org_id=<ORGID> TF_VAR_billing_account=<BILLINGID> terraform destroy 
