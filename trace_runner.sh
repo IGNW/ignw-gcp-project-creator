1c04291fa1733a8e120030d1f8f82d55645a57ec
Tom Chisholm <tomc@ignw.io>
2019-04-24 14:13:53 -0700

    changing billing account value

--- trace_runner.sh
+++ trace_runner.sh
@@ -3,4 +3,4 @@ export TF_LOG=TRACE
 export TF_LOG_PATH=./terraform.log
 cd terraform
 terraform init
-TF_VAR_org_id=943433058474 TF_VAR_billing_account=C9D630-DC571C terraform apply
+TF_VAR_org_id=943433058474 TF_VAR_billing_account=01160D-C9D630-DC571C terraform apply
