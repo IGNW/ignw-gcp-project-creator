06f1d67d85518f4d5cba150b533d5b6567761bb8
Tom Chisholm <tomc@ignw.io>
2019-04-24 16:34:28 -0700

    adding service key

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -23,15 +23,6 @@ resource "random_id" "random" {
    billing_account = "${var.billing_account}"
  }
 
-# Create the provisioner project
- #resource "google_project" "provisioner-project" {
- #  name = "provisioner-project"
- #  project_id      = "${random_id.random.dec}"
- #  org_id          = "${var.org_id}"
- #  billing_account = "${var.billing_account}"
- #}
-###############
-
 # Enable APIs
 
 # Cloud Resource Manager API: Creates, reads, and updates metadata for Google Cloud Platform resource containers.
@@ -62,11 +53,18 @@ resource "google_project_service" "cloud_billing_api" {
   disable_on_destroy = false
 }
 
+# END Enable APIs
+
 # Create the provisioner service account
 resource "google_service_account" "provisioner-svc" {
   account_id   = "provisioner-svc"
   display_name = "provisioner-svc"
   project      = "${google_project.provisioner-project.project_id}"
+}
+
+# Create a service account key
+resource "google_service_account_key" "provisioner" {
+  service_account_id = "${google_service_account.provisioner-svc.name}"
 }
 
 # Add the service account to the project
